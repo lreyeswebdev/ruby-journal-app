58e609313e2333c2ad3264858f09a81a0d8ccdd5
Ligaya Reyes <l.reyes062018@gmail.com>
2020-11-23 21:09:26 +0800

    added integration tests and edited views

---
+++ test/integration/categories_integration_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class CategoriesIntegrationTest < ActionDispatch::IntegrationTest
+    setup do
+        @category = categories(:one)
+    end
+
+    test 'should go to new category and create category' do
+        get new_category_path
+        assert_response :success
+
+        assert_difference 'Category.count', 1 do
+            post create_category_path, params: { category: {name: "create test", description: "integration test" }}
+            assert_response :redirect
+        end
+
+        follow_redirect!
+        assert_response :success
+
+    end
+
+    test 'should go to edit category and update category' do
+        get edit_category_url(@category)
+        assert_response :success
+
+        put update_category_url(@category), params: { category: { name: "update test", description: "integration test on update"}}
+        assert_response :redirect
+
+        follow_redirect!
+        assert_response :success
+    end
+
+end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,14 +1,15 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  test "should not save category without name" do
-    category = Category.new
-    assert_not category.save
-  end
-
+  
   def setup
     @category = Category.create(name: "Example")
   end
+
+  test "should not save category without name" do
+    category = Category.new
+    assert_not category.save
+  end  
 
   test "name should be unique" do
     duplicate_category = @category.dup
